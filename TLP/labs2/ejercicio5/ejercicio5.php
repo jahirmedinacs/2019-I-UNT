29b3475fb7916c0c82963fc51dcf17d2afbf9c39
DrSavage <services@jahirmedina.com>
2019-05-02 20:25:10 -0500

    v0.02.08.0000

--- TLP/labs2/ejercicio5/ejercicio5.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$base = $_POST['numero1'];
-	$potencia = $_POST['potencia'];
-
-	for ($i=0; $i <=$potencia; $i++) { 
-		echo $base."^".$i."&nbsp"."&nbsp"."&nbsp"."&nbsp"."&nbsp"."&nbsp";
-	}
- ?>
