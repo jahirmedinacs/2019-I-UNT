eefc215965de5ff21157a051f71aa5bb6c6b8736
DrSavage <services@jahirmedina.com>
2019-05-30 20:18:36 -0500

    v0.03.02.5555

--- TopSisop/u2-presentacion/py_demo/__proyectlib/dirtree.py
+++ TopSisop/u2-presentacion/py_demo/__proyectlib/dirtree.py
@@ -1,10 +1,53 @@
 #! /usr/bin/python3 -i
 
 import os
-from o_e_system import o_e_system as o_e_system
+#from o_e_system import o_e_system as o_e_system
+
+BLACKLIST=[
+]
+
+def is_in_blacklist(string):
+    global BLACKLIST
+    _blacklist = BLACKLIST
+    
+    for item in _blacklist:
+        if item in string:
+            return True
+        else:
+            pass
+
+    return False
+
+def clean_recursive_tree(current_dir):
+    
+    output = {}
+    
+    try:
+        sub_dirs = os.popen("ls -d " + current_dir + "/*/ 2>/dev/null").read().split("\n")[:-1]
+    except:
+        pass 
+    else:
+        for ref_subdir in sub_dirs:
+            temp_subdir = ref_subdir.replace(current_dir, "")[1:-1]
+            if len(temp_subdir) < 20: 
+                output[temp_subdir + "/"] = clean_recursive_tree(current_dir + "/" + temp_subdir)
+            else:
+                pass
+    
+    try:
+        sub_files = os.popen("ls -p " + current_dir  + " | grep -v / 2>/dev/null").read().split("\n")[:-1]
+    except:
+        pass
+    else:
+        for ref_subfile in sub_files: 
+            if len(ref_subfile) < 20:
+                output[ref_subfile] = None
+            else:
+                pass
+
+    return output
 
 def __recursive_tree(current_dir):
-    
     output = {}
     
     try:
@@ -26,14 +69,9 @@ def __recursive_tree(current_dir):
 
     return output
 
-
-
-
 def from_home_tree():
     home_root = os.popen('echo $HOME').read().strip()
     print(home_root) 
 
-#    print(__recursive_tree("/home/drsavage/Documents/gitrepos/2019-I-UNT-TLP/labs3"))
-    print(__recursive_tree(home_root))
+    return clean_recursive_tree(home_root)
 
-from_home_tree()
--- TopSisop/u2-presentacion/py_demo/main.py
+++ TopSisop/u2-presentacion/py_demo/main.py
@@ -1,9 +1,10 @@
 #! /usr/bin/python3 -s
 
-from "./__proyectlib" import dirtree.py
+from __proyectlib.dirtree import from_home_tree
+from pprint import pprint
 
-def main()
-
+def main():
+   pprint(from_home_tree())
 
 if __name__ == "__main__":
     main()
