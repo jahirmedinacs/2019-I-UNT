0c1ae16271aa76c1e8a7999ba502d4c55cdc67c8
DrSavage <services@jahirmedina.com>
2019-06-03 00:25:06 -0500

    v0.03.02.8888

--- TopSisop/u2-presentacion/py_demo/__proyectlib/dirtree.py
+++ TopSisop/u2-presentacion/py_demo/__proyectlib/dirtree.py
@@ -1,10 +1,16 @@
 #! /usr/bin/python3 -i
 
 import os
+import json
 #from o_e_system import o_e_system as o_e_system
 
 BLACKLIST=[
 ]
+
+__ref = open("./__proyectlib/whitelist.json", "r")
+WHITELIST=json.load(__ref)
+__ref.close()
+
 
 def is_in_blacklist(string):
     global BLACKLIST
@@ -18,10 +24,24 @@ def is_in_blacklist(string):
 
     return False
 
+def is_in_whitelist(string):
+    global WHITELIST
+    _whitelist = WHITELIST
+    
+    for item in _whitelist:
+        if item in string:
+            return True
+        else:
+            pass
+
+ 
+
 def clean_recursive_tree(current_dir):
     
     output = {}
     
+    #Carpetas
+
     try:
         sub_dirs = os.popen("ls -d " + current_dir + "/*/ 2>/dev/null").read().split("\n")[:-1]
     except:
@@ -29,18 +49,27 @@ def clean_recursive_tree(current_dir):
     else:
         for ref_subdir in sub_dirs:
             temp_subdir = ref_subdir.replace(current_dir, "")[1:-1]
+            # Primer Criterio : carpetas con nombres cortos
             if len(temp_subdir) < 20: 
-                output[temp_subdir + "/"] = clean_recursive_tree(current_dir + "/" + temp_subdir)
+                __temp = clean_recursive_tree(current_dir + "/" + temp_subdir)
+                # Segundo Criterio : No Carpetas Vacias (aun en revision)
+                if __temp == {}:
+                    pass
+                else:
+                    output[temp_subdir + "/"] = __temp
             else:
                 pass
     
+    #Archivos
+
     try:
         sub_files = os.popen("ls -p " + current_dir  + " | grep -v / 2>/dev/null").read().split("\n")[:-1]
     except:
         pass
     else:
         for ref_subfile in sub_files: 
-            if len(ref_subfile) < 20:
+            #Criterio: Archivos con nombres cortos y de formatos permitidos (basicos, definidos en la lista blanca)
+            if len(ref_subfile) < 20 and is_in_whitelist(ref_subfile):
                 output[ref_subfile] = None
             else:
                 pass
